b113d270dc1f53b2b75cf3ccee52fec6db8982b3
CalebPfaff <kalmarv@pm.me>
2021-12-29 15:53:16 -0800

    Added spinny circles 2, updates to use points instead of circles runs way faster

--- sketches/spinny_circles_2/sketch.js
+++ sketches/spinny_circles_2/sketch.js
@@ -1,5 +1,6 @@
 function setup() {
   angleMode(DEGREES);
+  colorMode(HSB);
   speed();
   ARCanvas(1, 1);
 
@@ -11,11 +12,12 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  background(0, 0.15);
 
   push();
   translate(width / 2, height / 2);
   for (var i = 0; i < runs; i++) {
+    stroke(map(i, 0, runs, 0, 450), 100, 100);
     point(i * spacing, 0);
     point(0, i * spacing);
     point(-i * spacing, 0);
