e68eb659d858e43fc548ba253a1b7c995abf38e8
CalebPfaff <kalmarv@pm.me>
2021-12-29 20:26:24 -0800

    Fixed digesting

--- sketches/digesting/sketch.js
+++ sketches/digesting/sketch.js
@@ -12,26 +12,29 @@ function draw() {
   randomSeed(seeed)
   noiseSeed(seeed)
   background(255)
-
   fill(0)
+
+  strokeWeight(px(0.25))
   noStroke()
+
   rect(px(5), px(90), px(90), px(5))
   rect(px(90), px(5), px(5), px(90))
 
   stroke(255)
-  //translate(m,m)
+
+  strokeWeight(px(0.1))
   for (let i = 0; i < runs; i++) {
     push()
-    xi = map(i, 0, runs, -100, w + 100)
-    yi = map(i, 0, runs, -100, w + 100)
+    xi = map(i, 0, runs, -px(10), w + px(20))
+    yi = map(i, 0, runs, -px(10), w + px(20))
     translate(
-      xi + sin(i / 10 + frameCount / 100) * 100,
-      yi + cos(i / 10 + frameCount / 100) * 100
+      xi + sin(i / 10 + frameCount / 100) * px(10),
+      yi + cos(i / 10 + frameCount / 100) * px(10)
     )
     rotate(i / 20 + frameCount / 100)
     ngon(
       2 + abs(noise(i / 20, frameCount / 200)) * 3,
-      50 + abs(noise(i / 10, frameCount / 100)) * 100
+      px(7) + abs(noise(i / 10, frameCount / 100)) * px(10)
     )
     pop()
   }
@@ -39,4 +42,11 @@ function draw() {
   noStroke()
   rect(px(5), px(5), px(90), px(5))
   rect(px(5), px(5), px(5), px(90))
+  stroke(255)
+  strokeWeight(px(0.25))
+  strokeCap(SQUARE)
+  line(px(5), px(5), px(95), px(5))
+  line(px(10), px(10), px(90), px(10))
+  line(px(5), px(5), px(5), px(95))
+  line(px(10), px(10), px(10), px(90))
 }
