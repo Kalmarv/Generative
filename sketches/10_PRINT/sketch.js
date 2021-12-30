4deb4d1745bd2bc8309a9a38397facd04da4b6cd
CalebPfaff <kalmarv@pm.me>
2021-12-29 16:48:38 -0800

    Added 10 Print

---
+++ sketches/10_PRINT/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  let params = getURLParams();
+  urlSeed(params);
+
+  speed();
+  ARCanvas(1, 1);
+
+  angleMode(DEGREES);
+  noLoop();
+
+  background(0);
+  stroke(255);
+  strokeWeight(px(0.5));
+
+  spacing = px(5);
+  grid = width / spacing;
+  orig = 0;
+}
+
+function draw() {
+  randomSeed(seeed);
+
+  for (var i = 0; i < grid; i++) {
+    for (var j = 0; j < grid; j++) {
+      prob()
+        ? line(spacing * j, orig, spacing * j + spacing, spacing + orig)
+        : line(spacing * j + spacing, orig, spacing * j, spacing + orig);
+    }
+    orig += spacing;
+  }
+}
